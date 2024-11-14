b9443e474340ef23d08c42ebab4166f5f197c769
Branko Rokov <brankorokov@outlook.com>
2024-11-13 21:45:45 -0500

    Adding animations to home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
+import { FaArrowCircleDown } from "react-icons/fa";
 
 export default function Home() {
     return (
         <section id="home">
             <div className="home-container bg-gradient-to-r from-customBlueGray to-customSlateGray text-white">
                 <div className="h-screen flex flex-col justify-center items-center text-center px-4">
-                    <div className="bg-customOffWhite p-8 md:p-12 lg:p-16 rounded-lg shadow-lg w-full max-w-2xl md:max-w-3xl lg:max-w-4xl">
-                        <h1 className="welcomeHeader text-5xl font-montserrat text-customBlack mb-5 font-bold">Branko Rokov</h1>
-                        <p className="welcomeText font-opensans text-customBlack text-2xl mb-7">Full Stack Developer</p>
-                        <button
-                            onClick={() => document.getElementById('about').scrollIntoView({ behavior: 'smooth' })}
-                            className="bg-customBlueGray hover:bg-customSlateGray text-white font-bold py-2 px-6 rounded-full welcomeButton font-montserrat transition duration-300 ease-in-out transform hover:scale-105"
-                        >
-                            Learn more
-                        </button>
+                    <div className="flex flex-col justify-center items-center -mt-56 animation">
+                        <h1 className="text-8xl md:text-9xl font-montserrat font-bold firstName">Branko</h1>
+                        <p className="lastName last-name text-8xl md:text-9xl font-montserrat">Rokov</p>
+                        <p className="text-xl md:text-2xl font-opensans mt-6 sub-title">Full Stack Developer</p>
+                        <FaArrowCircleDown onClick={() => document.getElementById('about').scrollIntoView({ behavior: 'smooth' })}
+                            className="rotate-in text-6xl mt-12 text-customOrange cursor-pointer pulse" />
                     </div>
                 </div>
             </div>
